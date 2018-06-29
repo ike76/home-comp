a71692686e3dab7f902b246e793074433008b1e2
ike76 <ike76@me.com>
2018-06-28 20:54:56 -0500

    begin material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
 import Layout from "./Layout/Layout";
 import "./App.css";
+
+const theme = createMuiTheme();
+
 export class App extends Component {
   render() {
-    return <Layout />;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Layout />
+      </MuiThemeProvider>
+    );
   }
 }
 
